eb15d7177000ca47137b2062b732683b4a7adc0a
TomFraser <tr.fraser@bigpond.com>
2019-11-14 16:10:26 +1000

    Now correctly works

--- PF_Motors.c
+++ PF_Motors.c
@@ -1,22 +1,22 @@
 #include "PF_Motors.h"
 
-#define MSS currentMotorSpeeds.t_motorControl_s // Shorthand for internal use
-#define MSA currentMotorSpeeds.t_motorControl_a // Shorthand for internal use
-#define MIS motorIn.t_motorControl_s // Shorthand for internal use
-#define MIA motorIn.t_motorControl_a // Shorthand for internal use
+#define MSS currentMotorSpeeds.structure // Shorthand for internal use
+#define MSA currentMotorSpeeds.array // Shorthand for internal use
+#define MIS motorIn.structure // Shorthand for internal use
+#define MIA motorIn.array // Shorthand for internal use
 
 void initPFMotors() {
-    MSS.A = 0;
-    MSS.B = 0;
-    MSS.C = 0;
-    MSS.D = 0;
+	MSS.A = 0;
+	MSS.B = 0;
+	MSS.C = 0;
+	MSS.D = 0;
 
-    MIS.A = 0;
-    MIS.B = 0;
-    MIS.C = 0;
-    MIS.D = 0;
+	MIS.A = 0;
+	MIS.B = 0;
+	MIS.C = 0;
+	MIS.D = 0;
 
-    initFlag = 1;
+	initFlag = 1;
 }
 
 int getPFMotorSpeed(int motor) {
@@ -24,41 +24,46 @@ int getPFMotorSpeed(int motor) {
 }
 
 task updateMotors() {
-    for(int i = 0; i < MAX_MOTORS; i++) {
-        // For each motor, update speed if a change is detected
-		if(abs(MIA[i] - MSA[i]) > 1) {
-			//Different speed, update.
-			motor[mArray[i]] = MSA[i];
-			MSA[i] = MIA[i];
-			} else {
-            // Update motor speeds
-			if(pulsed[i]) {
-                // We are pulsed, go to requested speed
-				motor[mArray[i]] = MSA[0];
-				} else {
-				motor[mArray[i]] = MSA[0] - 1;
+	for(;;) {
+		for(int i = 0; i < 4; i++) {
+			// For each motor, update speed if a change is detected
+			if(abs(MIA[i] - MSA[i]) > 1) {
+				//Different speed, update.
+				motor[mArray[i]] = MSA[i];
+				MSA[i] = MIA[i];
 			}
-            pulsed[i] = !pulsed[i];
+			// Update motor speeds
+			if(pulsed[i]) {
+				// We are pulsed, go to requested speed
+				motor[mArray[i]] = MSA[i];
+				} else {
+				if(MSA[i] > 0) {
+					motor[mArray[i]] = MSA[i] - 1;
+				} else {
+					motor[mArray[i]] = MSA[i] + 1;
+				}
+			}
+			pulsed[i] = !pulsed[i];
 		}
-    }
+	}
 }
 
 void startMotorTask() {
-    if(initFlag < 1) {
-        printf("Hey! Init the motor library! \n");
-    } else {
-        startTask(updateMotors);
-    }
+	if(initFlag < 1) {
+        // Not printf in RobotC yikes dawg
+		} else {
+		startTask(updateMotors);
+	}
 }
 
 void exampleSetSpeedFunction() {
-    // Set MotorA speed to 100:
-    motorIn.t_motorControl_s.A = 100;   // <--
-    // Or                               Both these functions set the same address in memory, they are just different ways of addressing it.
-    motorIn.t_motorControl_a[0] = 100;  // <--
+	// Set MotorA speed to 100:
+	motorIn.structure.A = 100;   // <--
+	// Or                               Both these functions set the same address in memory, they are just different ways of addressing it.
+	motorIn.array[0] = 100;  // <--
 
-    // Set MotorB speed to -50:
-    motorIn.t_motorControl_s.B = -50;   // <--
-    // Or                               Both these functions set the same address in memory, they are just different ways of addressing it.
-    motorIn.t_motorControl_a[1] = -50;  // <--
+	// Set MotorB speed to -50:
+	motorIn.structure.B = -50;   // <--
+	// Or                               Both these functions set the same address in memory, they are just different ways of addressing it.
+	motorIn.array[1] = -50;  // <--
 }
--- PF_Motors.h
+++ PF_Motors.h
@@ -3,21 +3,23 @@
 
 #define MAX_MOTORS 4
 
+typedef struct motorControl_s {
+		int A, B, C, D;
+} t_motorControl_s;
+
 typedef union motorControl_u {
-    typedef struct motorControl_s {
-        int A, B, C, D;
-    } t_motorControl_s;
-    int t_motorControl_a[3];
+    t_motorControl_s structure;
+    int array[4];
 } t_motorControl;
 
 t_motorControl motorIn; // Set motor speeds for all motors
 t_motorControl currentMotorSpeeds; // Holds the current motor speeds
 
-bool pulsed[4] = {false};
+bool pulsed[4] = false;
 
 int initFlag = 0;
 
-t_motor mArray[4] = {motorA, motorB, motorC, motorD};
+tMotor mArray[4] = {motorA, motorB, motorC, motorD};
 
 void initPFMotors(); // Init PF motors
 
@@ -25,7 +27,7 @@ int getPFMotorSpeed(int motor); // Get speed of each motor
 
 task updateMotors(); // Pulse speeds
 
-void startMotorsTask(); // Start the motor task
+void startMotorTask(); // Start the motor task
 
 void exampleSetSpeedFunction(); // An example function on how to set motor speeds in your main loop / program
 
